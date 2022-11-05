02d3e0115e6e139bbeff56654674edc89d772ab7
Takatada Yoshima <yoshima@topotal.com>
2022-11-05 17:37:18 +0900

    Fix type error in layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
 import styles from './layout.module.css';
 
-export default function Layout({ children }) {
+export default function Layout({ children }: {children: any}) {
   return <div className={styles.container}>{children}</div>;
 }
